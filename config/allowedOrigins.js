f9f69417554dbcd67ef93950ad9b6a29371afed0
ahlem20 <107472826+ahlem20@users.noreply.github.com>
2024-03-16 14:14:48 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'https://www.dandrepairshop.com',
-    'https://technotes.onrender.com'
+    'https://qrrcr.onrender.com'
     ]
     
     module.exports = allowedOrigins
